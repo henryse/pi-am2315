aaaeafed76d4f9bb56b0917c889c798bf47175a0
henryse <henryse@hotwire.com>
2016-10-16 20:37:27 -0700

    Added in some more fixes.

--- src/pi_am2315.c
+++ src/pi_am2315.c
@@ -1,6 +1,29 @@
+/**********************************************************************
+//    Copyright (c) 2016 Henry Seurer & Samuel Kelly
 //
-// Created by Henry Seurer on 10/16/16.
+//    Permission is hereby granted, free of charge, to any person
+//    obtaining a copy of this software and associated documentation
+//    files (the "Software"), to deal in the Software without
+//    restriction, including without limitation the rights to use,
+//    copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the
+//    Software is furnished to do so, subject to the following
+//    conditions:
 //
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//    OTHER DEALINGS IN THE SOFTWARE.
+//
+**********************************************************************/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
@@ -122,7 +145,7 @@ bool pi_am2315_readTemperatureAndHumidity(int fd, float *temperature, float *hum
 
     // send the read request
     //
-    n = write(fd, read_request, 3);
+    n = write(fd, read_request, AM2315_READREG);
 
     printf("write returned %d bytes\n",n);
 
@@ -154,6 +177,3 @@ bool pi_am2315_readTemperatureAndHumidity(int fd, float *temperature, float *hum
 
     return crc_res == crc;
 }
-
-
-/*********************************************************************/
